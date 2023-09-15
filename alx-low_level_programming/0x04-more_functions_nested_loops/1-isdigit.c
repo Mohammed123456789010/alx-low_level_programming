d00052f1429c5d7c84eea9119ba8d2351f7c437f
Mohammed123456789010 <aboelngam138@icloud.com>
2023-09-14 17:44:05 -0700

    isdight

--- alx-low_level_programming/0x04-more_functions_nested_loops/1-isdigit.c
+++ alx-low_level_programming/0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isdigit - check if numbers are 0 - 9
  * @c: char to be checked
@@ -9,7 +10,7 @@
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-	return (1);
+		return (1);
 	else
-	return (0);
+		return (0);
 }
